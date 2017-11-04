dd591f9d3d245e0c4b6905aafa8e4760b4c312d0
HubenkoVV <hubenkovlada@gmail.com>
2017-11-04 14:22:21 +0200

    Second variant, with properties

---
+++ 2nd Task (right variant)/src/ua/training/Main.java
@@ -0,0 +1,9 @@
+package ua.training;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Controller controller = new Controller(new Model(),new View());
+        controller.processUser();
+    }
+}
