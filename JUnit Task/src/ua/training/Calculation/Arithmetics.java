60adb44f740192082ccc6c454a03502a95657ec1
HubenkoVV <hubenkovlada@gmail.com>
2017-11-06 21:46:08 +0200

    JUnit Task

---
+++ JUnit Task/src/ua/training/Calculation/Arithmetics.java
@@ -0,0 +1,24 @@
+package ua.training.Calculation;
+
+/**
+ * Created by Влада on 01.11.2017.
+ */
+public class Arithmetics {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double deduct(double a, double b) {
+        return a - b;
+    }
+
+    public double mult(double a, double b) {
+        return a * b;
+    }
+
+    public double div(int a, int b) {
+        return a/b;
+    }
+
+}
